e23d49d60348621a4ec3258f6ea74bc3635ceff3
yhsprice <yhsprice@hotmail.com>
2026-05-19 08:21:52 -0400

    Add interest questions to interests.js

---
+++ interests.js
@@ -0,0 +1,28 @@
+questions.easy.push(
+  {
+    sectionId: "careerRealityFit",
+    section: "Interest Snapshot",
+    type: "interest",
+    question: "Which activity sounds most interesting to you?",
+    options: [
+      "Fixing or building something",
+      "Helping someone solve a problem",
+      "Designing something creative",
+      "Finding patterns in information"
+    ],
+    interestMap: ["handsOn", "helpingPeople", "creative", "data"]
+  },
+  {
+    sectionId: "careerRealityFit",
+    section: "Interest Snapshot",
+    type: "interest",
+    question: "Which work setting sounds best?",
+    options: [
+      "Outside or moving around",
+      "Quiet and focused",
+      "With a team",
+      "Using technology"
+    ],
+    interestMap: ["active", "independent", "teamBased", "technology"]
+  }
+);
