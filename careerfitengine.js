9fc1983c060c66d7fca08e884281de332a235902
yhsprice <yhsprice@hotmail.com>
2026-05-21 07:51:27 -0400

    Create careerfitengine.js

---
+++ careerfitengine.js
@@ -0,0 +1,40 @@
+const careerFitRules = {
+  healthcare: [
+    "helpingPeople",
+    "readingComprehension",
+    "attentionToDetail",
+    "science",
+    "active"
+  ],
+
+  technology: [
+    "logic",
+    "patternRecognition",
+    "technology",
+    "data",
+    "problemSolving"
+  ],
+
+  trades: [
+    "handsOn",
+    "mechanicalReasoning",
+    "spatialReasoning",
+    "practicalThinking",
+    "active"
+  ],
+
+  business: [
+    "mathReasoning",
+    "communication",
+    "organization",
+    "data",
+    "problemSolving"
+  ],
+
+  creative: [
+    "creativity",
+    "ideaGeneration",
+    "visualThinking",
+    "communication"
+  ]
+};
